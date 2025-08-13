63b6206fc8ba1048ca4681a1289ac86cf5b24265
Peter stromdahl <110845677+Stpeterd@users.noreply.github.com>
2025-08-13 16:13:44 -0500

    Add files via upload

---
+++ quiz_game.py
@@ -0,0 +1,40 @@
+# typing along with a youtube video
+print("welcome to my computer game ")
+
+playing = input("do you want to play?")
+
+if playing != "yes":
+    quit()
+
+print("Okay lets play :)")
+
+score = 0
+
+answer = input("what does CPU stand for? ")
+if answer.lower()== "central processing unit":
+    print("Correct!")
+score += 1
+
+print("Incorrect")
+
+answer = input("what does RAM stand for?")
+if answer.lower() ==  "random access memory":
+    print("Correct!")
+    score += 1
+
+else:
+    print("Incorrect!")
+answer = input("what does PSU stand for?")
+if answer.lower() == "power supply":
+
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect! ")
+
+print("you got " + str(score) + "questions correct!")
+print("you got " + str(score/4 * 1) + " questions correct!")
+
+
+
+
