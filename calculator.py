0e48a03dbce4f6c08adbb8aa66b8aa88abb0601e
peter <stromdahl2007@gmail.com>
2025-08-14 20:32:41 -0500

    code

--- calculator.py
+++ calculator.py
@@ -45,3 +45,82 @@ for row in range(row_count):
                 button.grid(row=row+1, column=column)
 
 frame.pack()
+
+#A+B, A-B, A*B, A/B
+A = "0"
+operator = None
+B = None
+
+#A+B, A-B, A*B, A/B
+A = "0"
+operator = None
+B = None
+
+def button_clicked(value):
+    global right_symbols, top_symbols, label, A, B, operator
+
+    if value in right_symbols:
+        if value == "=":
+            if A is not None and operator is not None:
+                B = label["text"]
+                numA = float(A)
+                numB = float(B)
+
+                if operator == "+":
+                    label["text"] = remove_zero_decimal(numA + numB)
+                elif operator == "-":
+                    label["text"] = remove_zero_decimal(numA - numB)
+                elif operator == "×":
+                    label["text"] = remove_zero_decimal(numA * numB)
+                elif operator == "÷":
+                    label["text"] = remove_zero_decimal(numA / numB)
+                
+                clear_all()
+
+            elif value in "+-×÷": #500 +, *
+            
+                if operator is None:
+                    A = label["text"]
+                    label["text"] = "0"
+                    B = "0"
+            
+            operator = value
+
+    elif value in top_symbols:
+        if value == "AC":
+            clear_all()
+            label["text"] = "0"
+
+        elif value == "+/-":
+            result = float(label["text"]) * -1
+            label["text"] = remove_zero_decimal(result)
+
+        elif value == "%":
+            result = float(label["text"]) / 100
+            label["text"] = remove_zero_decimal(result)           
+        
+    else: #digits or .
+        if value == ".":
+            if value not in label["text"]:
+                label["text"] += value
+
+        elif value in "0123456789":
+            if label["text"] == "0":
+                label["text"] = value #replace 0
+            else:
+                label["text"] += value #append digit
+
+#center the window
+window.update() #update window with the new size dimensions
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+window_x = int((screen_width/2) - (window_width/2))
+window_y = int((screen_height/2) - (window_height/2))
+
+#format "(w)x(h)+(x)+(y)"
+window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
+
+window.mainloop()
