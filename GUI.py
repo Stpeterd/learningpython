0275fb512e4cd83afc483e09bac266c95ad513f9
Peter stromdahl <110845677+Stpeterd@users.noreply.github.com>
2025-08-20 23:54:46 -0500

    cal

---
+++ GUI.py
@@ -0,0 +1,20 @@
+# tkinter GUI 
+import  tkinter as tk
+
+calculation = ""
+
+def add_to_calculation(symbol):
+    pass
+
+def evalute_calculation():
+    pass
+def clear_field():
+    pass
+
+root = tk.Tk()
+root.geometry("300x275")
+
+text_result = tk.Text(root, height=2, width=16, font=("Arial", 24))
+
+text_result.grid(columnspan=5)
+root.mainloop()
