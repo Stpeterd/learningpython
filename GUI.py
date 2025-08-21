4b0f726442f509a2ae939b8cde937a7c454ce782
Peter stromdahl <110845677+Stpeterd@users.noreply.github.com>
2025-08-21 12:07:41 -0500

    add butn

--- GUI.py
+++ GUI.py
@@ -26,7 +26,7 @@ def clear_field():
 
     
 root = tk.Tk()
-root.geometry("300x275")
+root.geometry("300x300")
 
 text_result = tk.Text(root, height=2, width=16, font=("Arial", 24))
 
@@ -55,7 +55,18 @@ btn_8 = tk.Button(root, text="8", command=lambda: add_to_calculation(8), width=5
 btn_8.grid(row=4, column=2)
 btn_9 = tk.Button(root, text="9", command=lambda: add_to_calculation(9), width=5, font=("Arial", 14))
 btn_9.grid(row=4, column=3)
+btn_9.grid(row=3, column=3)
 btn_0 = tk.Button(root, text="0", command=lambda: add_to_calculation(0), width=5, font=("Arial", 14))
 btn_0.grid(row=5, column=2)
+btn_plus = tk.Button(root, text="+", command=lambda: add_to_calculation("+"), width=5, font=("Arial", 14))
+btn_plus.grid(row=2, column=4)
+btn_minus = tk.Button(root, text="-", command=lambda: add_to_calculation("-"), width=5, font=("Arial", 14))
+btn_minus.grid(row=3, column=4)
+btn_mul = tk.Button(root, text="*", command=lambda: add_to_calculation("*"), width=5, font=("Arial", 14))
+btn_mul.grid(row=4, column=4)
+btn_div = tk.Button(root, text="/", command=lambda: add_to_calculation("/"), width=5, font=("Arial", 14))
+btn_div.grid(row=5, column=4)
+
+
 
 root.mainloop()
